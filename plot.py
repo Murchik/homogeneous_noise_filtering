1d7e6b7b5d052b828fc8bb5c69b83f78c48b8c6b
Murchik <mur4ikoff@gmail.com>
2022-05-20 09:27:10 +0300

    updating graphs output

--- plot.py
+++ plot.py
@@ -5,6 +5,9 @@ import librosa
 import librosa.display
 
 
+figure_size = (12, 4)
+
+
 def signal(signal, sample_rate, title="signal"):
     # Вычисление длительности сигнала:
     # длительность = кол-во_семплов / кол-во_семплов_в_секунду
@@ -15,7 +18,8 @@ def signal(signal, sample_rate, title="signal"):
     # Вывод результата
     plt.figure(figsize=(18, 5))
     plt.plot(time, signal)
-    plt.xlabel("time")
+    plt.ylabel("Амплитуда")
+    plt.xlabel("Время (Секунды)")
     plt.title(title)
     plt.show()
 
@@ -35,16 +39,33 @@ def magnitude_spectrum(signal, sample_rate,
     num_frequency_bins = int(len(frequency) * f_ratio)
 
     # Вывод результата
-    plt.figure(figsize=(18, 5))
+    plt.figure(figsize=figure_size)
     plt.plot(frequency[:num_frequency_bins], magnitude[:num_frequency_bins])
-    plt.xlabel("Hz")
+    plt.ylabel("Величина")
+    plt.xlabel("Частота (Гц)")
     plt.title(title)
     plt.show()
 
 
-def spectrogram(signal, sample_rate,
-                y_axis="log",
-                frame_size=2048, hop_length=512):
+def spectrogram(signal, title):
+    figure, x_axis = plt.subplots(figsize=figure_size)
+    color_x_axis = x_axis.matshow(
+        signal,
+        origin="lower",
+        aspect="auto",
+        cmap=plt.cm.seismic,
+        vmin=-1 * np.max(np.abs(signal)),
+        vmax=np.max(np.abs(signal)),
+    )
+    plt.colorbar(color_x_axis)
+    plt.xlabel(title)
+    plt.tight_layout()
+    plt.show()
+
+
+def spectrogram_from_signal(signal, sample_rate,
+                            y_axis="log",
+                            frame_size=2048, hop_length=512):
     # Получение матрицы распределения спектров во времени (спектрограммы):
     # по сигналу перемещается окно размером frame_size с шагом hop_size,
     # к каждому полученному таким окном фрейму применяется БПФ
@@ -58,11 +79,14 @@ def spectrogram(signal, sample_rate,
     y_log_scale = librosa.power_to_db(y_scale)
 
     # Вывод результата
-    plt.figure(figsize=(18, 5))
+    plt.figure(figsize=figure_size)
     librosa.display.specshow(y_log_scale,
                              sr=sample_rate,
                              hop_length=hop_length,
                              x_axis="time",
                              y_axis=y_axis)
     plt.colorbar(format="%+2.f")
+    plt.ylabel("Частота (Гц)")
+    plt.xlabel("Время (Секунды)")
+    plt.tight_layout()
     plt.show()
